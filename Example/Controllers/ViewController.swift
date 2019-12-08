5fbdf9c83fbdbbe2bd2dc597ea73f9bfbfd20ffc
Ivan Vorobei <ivanvorobei@icloud.com>
2019-12-08 21:08:18 +0300

    Add .bolt preset

--- Example/Controllers/ViewController.swift
+++ Example/Controllers/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UITableViewController {
         Alert(key: "Exclamation", preset: .exclamation, title: "Oops", subtitle: "Something went wrong"),
         Alert(key: "Question", preset: .question, title: "Oops", subtitle: "Form not complete"),
         Alert(key: "Message", preset: .message, title: "Message sended", subtitle: "You can check history"),
+        Alert(key: "Bolt", preset: .bolt, title: "Power", subtitle: "Now turned on"),
         Alert(key: "Add", preset: .add, title: "Added to Folder", subtitle: nil),
         Alert(key: "Error", preset: .error, title: "Oops", subtitle: "Please try again later"),
         Alert(key: "Shuffle", preset: .shuffle, title: "Songs Shuffled", subtitle: nil),
